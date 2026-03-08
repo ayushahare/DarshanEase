4212183c966bf0a4ae839fc40c63d39b018ebcde
Ayush Shahare <shahareayush123@gmail.com>
2026-03-08 17:03:45 +0530

    Fix CORS for production

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://darshanease.vercel.app'  
+    'https://darshanease1.vercel.app'  
   ],
   credentials: true
 }));
